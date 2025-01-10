a7b1b4a0595b3ec072096a46bf5b73d67a8084c9
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-01-10 15:59:26 +0530

    Basics
    Variables
    Constants
    in Golang

---
+++ 1.Basics/Variables.go
@@ -0,0 +1,20 @@
+package basics
+
+import "fmt"
+
+func Variables() {
+	var a = "initial"
+	fmt.Println(a)
+
+	//Data Type Declartion
+	var c, b int = 1, 2
+	fmt.Println(c, b)
+
+	var d = true
+	fmt.Println(d)
+
+	//Short Hand method of declaration
+	f := "a"
+	fmt.Println(f)
+
+}
