ee73dad6100214a37b05861fdb29bf4419c777df
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-06-02 20:57:18 +0530

    Introduction
    Constants in Golang

---
+++ Introduction/Constants.go
@@ -0,0 +1,21 @@
+package Introduction
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func Constants() {
+	fmt.Println(s)
+
+	const n = 50000
+
+	const d = 3e20 / n
+	fmt.Println(d)
+
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+}
